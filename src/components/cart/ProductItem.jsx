b99d6bce261330d53d0aabe2dc878b80636bdc09
Zufarbek2002 <game123qwerty00@gmail.com>
2024-03-20 14:00:08 +0500

    some changed

--- src/components/cart/ProductItem.jsx
+++ src/components/cart/ProductItem.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "../../SASS/cart/Cart.scss";
-import delImg from '../../assets/images/del_img.svg'
+import delImg from "../../assets/images/del_img.svg";
 
 const ProductItem = () => {
   const [products, setProducts] = useState([]);
@@ -33,11 +33,13 @@ const ProductItem = () => {
   useEffect(() => {
     localStorage.setItem("products", JSON.stringify(products));
   });
+  let sum = products.map(pr=>pr.price*pr.count).reduce((a,b)=> a+b,0)
+  console.log(sum)
 
   return (
     <div className="container product_item__container">
       <table>
-        <thead >
+        <thead>
           <tr className="table__head">
             <th></th>
             <th>Product</th>
@@ -50,32 +52,57 @@ const ProductItem = () => {
         <tbody>
           {products.map((product) => (
             <tr className="table_product" key={product.id}>
-                <td>
+              <td>
                 <div className="product_item_img">
                   <img src={product.image} alt="" />
                 </div>
-                </td>
-                <td><h2>{product.name}</h2></td>
-                <td><h4>Rp {product.price}</h4></td>
-                <td>
+              </td>
+              <td>
+                <h2>{product.name}</h2>
+              </td>
+              <td>
+                <h4>Rp {product.price}</h4>
+              </td>
+              <td>
                 <div className="btn_box">
                   <button onClick={() => handleDecrement(product.id)}>-</button>
                   <p className="product_count">{product.count}</p>
                   <button onClick={() => hadleIncrement(product.id)}>+</button>
                 </div>
-                </td>
-                <td>
+              </td>
+              <td>
                 <h4>Rp {product.price * product.count}.00</h4>
-                </td>
-                <td>
-                <button className="del_btn" onClick={() => hadleDelete(product.id)}>
+              </td>
+              <td>
+                <button
+                  className="del_btn"
+                  onClick={() => hadleDelete(product.id)}
+                >
                   <img src={delImg} alt="" />
                 </button>
-                </td>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="card_costs_box">
+        <h2>Cart Totals</h2>
+        <div className="card_costs">
+          {
+            products.map(product=>(
+              <div className="cost_box" key={product.id}>
+                <h3>Subtotal</h3>
+                <p>Rs. {product.price * product.count}.00</p>
+              </div>
+            ))
+          }
+        </div>
+        <div className="cost_box">
+          <h3>Total</h3>
+          <p>{sum}</p>
+        </div>
+        <button className="product__btn">Check Out</button>
+      </div>
     </div>
   );
 };
